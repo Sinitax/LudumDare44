da2c42ebf53463c963a76e1cbbe47ea03ef04055
Alexander <alexander0153@users.noreply.github.com>
2019-04-29 01:47:01 +0200

    Make cutscenes skippable

--- core/src/com/ludumdare44/game/Cutscenes/CutsceneScreen.java
+++ core/src/com/ludumdare44/game/Cutscenes/CutsceneScreen.java
@@ -1,11 +1,16 @@
 package com.ludumdare44.game.Cutscenes;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Align;
+import com.ludumdare44.game.Controls.ControlManager;
 import com.ludumdare44.game.Cutscenes.callbacks.ICutsceneCompleteListener;
 import com.ludumdare44.game.GFX.GFXManager;
 
@@ -16,6 +21,7 @@ import java.util.Queue;
 
 public class CutsceneScreen implements Screen {
 
+    protected ControlManager controlManager;
     protected GFXManager gfxManager;
     protected final ShapeRenderer shapeRenderer = new ShapeRenderer();
 
@@ -25,7 +31,12 @@ public class CutsceneScreen implements Screen {
     protected final List<CutsceneCharacter> cutsceneCharacters = new ArrayList<>();
     protected final List<ICutsceneCompleteListener> completeListeners = new ArrayList<>();
 
+    protected float skipLabelAlpha = 0;
+
     public CutsceneScreen(Color backgroundColor, boolean cinematicBars) {
+        controlManager = new ControlManager();
+        Gdx.input.setInputProcessor(controlManager);
+
         this.backgroundColor = backgroundColor;
         this.cinematicBars = cinematicBars;
     }
@@ -47,6 +58,17 @@ public class CutsceneScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        // Input handling
+        controlManager.update();
+        if(controlManager.justPressed(Input.Keys.ESCAPE)) {
+            if(skipLabelAlpha <= 0) {
+                skipLabelAlpha = 2;
+            } else {
+                cutsceneEvents.clear();
+            }
+        }
+        skipLabelAlpha -= delta;
+
         // Cutscene processing
         if(cutsceneEvents.isEmpty()) {
             for(ICutsceneCompleteListener listener : completeListeners)
@@ -83,6 +105,16 @@ public class CutsceneScreen implements Screen {
             shapeRenderer.rect(0, 0, gfxManager.screenSize.x, gfxManager.screenSize.y*marginPercent);
             shapeRenderer.rect(0, (1-marginPercent)*gfxManager.screenSize.y, gfxManager.screenSize.x, gfxManager.screenSize.y*marginPercent);
             shapeRenderer.end();
+        }
+
+        // Render skip label
+        if(skipLabelAlpha > 0) {
+            BitmapFont font = new BitmapFont();
+            gfxManager.batch.setColor(1, 1, 1, MathUtils.clamp(skipLabelAlpha, 0, 1));
+            gfxManager.batch.begin();
+            font.draw(gfxManager.batch, "Press ESC to skip", gfxManager.screenSize.x*0.9f, gfxManager.screenSize.x*0.1f, 0, Align.right, false);
+            gfxManager.batch.end();
+            gfxManager.batch.setColor(1, 1, 1, 1);
         }
 
         currentEvent.postRender(delta, this, gfxManager);
