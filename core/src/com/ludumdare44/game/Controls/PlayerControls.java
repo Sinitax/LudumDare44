d5abf49c29e7ef9dd1362e55a6305b5b5106b828
Sinitax <quent.burda@gmail.com>
2019-04-29 12:17:43 +0200

    shift + space movement

--- core/src/com/ludumdare44/game/Controls/PlayerControls.java
+++ core/src/com/ludumdare44/game/Controls/PlayerControls.java
@@ -11,22 +11,30 @@ public class PlayerControls {
     private ControlManager controlManager;
     private CameraManager cameraManager;
 
+    private float grappleTimer = 0;
+    private float reelCooldown = 200;
 
     public void update(float delta) {
         if (!player.isBusy()) {
-            if (controlManager.justPressed(Input.Keys.E)) {
+            if (controlManager.justPressed(Input.Keys.SPACE)) {
+                grappleTimer = 0;
                 player.stopGrapple();
                 Vector2 mouseVec = new Vector2(Gdx.input.getX(), cameraManager.getScreenSize().y - Gdx.input.getY()).add(cameraManager.getPos()).sub(cameraManager.getScreenSize().scl(0.5f));
                 player.doGrapple(mouseVec);
-            } else if (controlManager.justPressed(Input.Keys.R)) {
-                player.doAbility();
-            } else if (controlManager.justPressed(Input.Keys.SPACE) && player.isGrappling()) {
-                player.doJump();
             }
-            if (controlManager.justPressed(Input.Keys.W)) {
-                player.doReel();
-            } else if (controlManager.justReleased(Input.Keys.W)) {
+            /*
+            if (controlManager.justPressed(Input.Keys.R)) {
+                player.doAbility();
+            }
+            */
+            if (controlManager.justPressed(Input.Keys.SHIFT_LEFT) && player.isGrappling()) {
+                player.doJump();
+            } else if (controlManager.justReleased(Input.Keys.SPACE)) {
                 player.stopReel();
+            }
+            if (controlManager.isPressed(Input.Keys.SPACE)) {
+                grappleTimer += delta;
+                if (grappleTimer > reelCooldown) player.doReel();
             }
 
         }
