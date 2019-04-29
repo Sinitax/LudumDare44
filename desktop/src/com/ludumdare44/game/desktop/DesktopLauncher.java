a495c6ab5efc3542ebb24ccf51f03eb0b4d1477a
Alexander <alexander0153@users.noreply.github.com>
2019-04-29 20:00:03 +0200

    Add icon

--- desktop/src/com/ludumdare44/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ludumdare44/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.ludumdare44.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ludumdare44.game.Constants;
@@ -11,6 +12,9 @@ public class DesktopLauncher {
 		config.height = 600;
 		config.width = 800;
 		config.title = Constants.GAME_TITLE;
+		config.addIcon("assets/icons/128.png", Files.FileType.Internal);
+		config.addIcon("assets/icons/32.png", Files.FileType.Internal);
+		config.addIcon("assets/icons/16.png", Files.FileType.Internal);
 		new LwjglApplication(new LudumDareGame(), config);
 	}
 }
